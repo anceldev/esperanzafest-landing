bdd52859273018ee144e0526df513eae74061646
Ancel Guarayo <aguarayo@gmail.com>
2024-06-02 19:25:58 +0200

    Pre vimeo package

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'white': '#ffffff',
+			'darkBlue': '#03024c',
+		}
 	},
 	plugins: [],
 }
