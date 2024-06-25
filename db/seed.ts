79156e0279b4e58291b04ecdf570e06db4121eab
Ancel Guarayo <aguarayo@gmail.com>
2024-06-25 16:46:18 +0200

    Pre deploy to vercel

--- db/seed.ts
+++ db/seed.ts
@@ -3,8 +3,8 @@ import { db, Contact } from 'astro:db';
 // https://astro.build/db/seed
 export default async function() {
 	await db.insert(Contact).values({
-		name: "Name",
-		email: 'name@mail.com',
-		phone: 'phone number'
+		name: "Prueba",
+		email: 'prueba@mail.com',
+		phone: 'teléfono'
 	})
 }
